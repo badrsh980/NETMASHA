f1b7e13935a525d35e9273ef2e15f66b8b1980a0
Bader Al-Harbi <60719639+badrsh980@users.noreply.github.com>
2023-12-27 14:43:16 +0300

    bader

--- netmasha/lib/main.dart
+++ netmasha/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:netmasha/blocs/auth_bloc/auth_bloc.dart';
-import 'package:netmasha/blocs/onbaording_bloc/onbaording_bloc.dart';
+import 'package:netmasha/blocs/auth_bloc/auth_state.dart';
 import 'package:netmasha/screens/nav_bar.dart';
+import 'package:netmasha/screens/onboarding.dart';
 import 'package:netmasha/screens/splash_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
@@ -15,49 +15,55 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  Future<bool> _checkIfLoggedIn() async {
+  Future<String> _getToken() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    String token = prefs.getString("token") ?? "";
-    return token.isNotEmpty;
+    return prefs.getString("token") ?? "";
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).requestFocus(FocusNode());
-      },
-      child: MultiBlocProvider(
-        providers: [
-          BlocProvider<OnbaordingBloc>(
-              create: (BuildContext context) => OnbaordingBloc()),
-          BlocProvider<AuthBloc>(create: (BuildContext context) => AuthBloc()),
-          // Add other BlocProviders if needed
+    return BlocProvider<AuthBloc>(
+      create: (context) => AuthBloc(),
+      child: MaterialApp(
+        theme: ThemeData(fontFamily: "IBM Plex Sans Arabic"),
+        locale: const Locale('ar'),
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
         ],
-        child: MaterialApp(
-          theme: ThemeData(fontFamily: "IBM Plex Sans Arabic"),
-          locale: const Locale('ar'),
-          localizationsDelegates: const [
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [Locale('ar')],
-          debugShowCheckedModeBanner: false,
-          home: FutureBuilder<bool>(
-            future: _checkIfLoggedIn(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.data == true) {
-                  return NavBar();
-                } else {
-                  return const SplashScreen();
-                }
-              } else {
-                return const CircularProgressIndicator();
-              }
-            },
-          ),
+        supportedLocales: const [Locale('ar')],
+        debugShowCheckedModeBanner: false,
+        home: FutureBuilder<String>(
+          future: _getToken(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return BlocListener<AuthBloc, AuthState>(
+                listener: (context, state) {
+                  if (state is AuthLoginSuccessState ||
+                      state is AuthOTPSuccessState) {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => NavBar()),
+                      (route) => false,
+                    );
+                  } else if (state is AuthLoginErrorState ||
+                      state is AuthOTPErrorState ||
+                      state is AuthRegisterErrorState) {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => Onboarding()),
+                      (route) => false,
+                    );
+                  }
+                },
+                child:
+                    snapshot.data!.isNotEmpty ? NavBar() : const SplashScreen(),
+              );
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
         ),
       ),
     );
